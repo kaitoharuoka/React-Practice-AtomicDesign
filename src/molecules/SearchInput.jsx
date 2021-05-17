c770991ee1a8ddec5f45114db3f330bcd3e5b53b
kaitoharuoka <73736001+kaitoharuoka@users.noreply.github.com>
2021-05-17 20:00:04 +0900

    Moleculeの作成

---
+++ src/molecules/SearchInput.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { PrimaryButton } from "../components/atoms/button/PrimaryButton";
+import { Input } from "../components/atoms/input/input";
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <SContainer>
+        <Input placeholder="検索条件を入力" />
+        <SButtonWrapper>
+          <PrimaryButton>検索</PrimaryButton>
+        </SButtonWrapper>
+      </SContainer>
+    </div>
+  );
+};
+
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
