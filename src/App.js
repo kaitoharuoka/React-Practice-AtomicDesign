7fe725b698bf497270ebe85e11dd92e232c4495e
kaitoharuoka <73736001+kaitoharuoka@users.noreply.github.com>
2021-05-19 18:27:18 +0900

    Organismの作成2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { UserCard } from "./components/organisms/user/UserCard";
 import { SearchInput } from "./molecules/SearchInput";
 import "./styles.css";
 
+const user = {
+  name: "かいと",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@example.com",
+  phone: "080-1212-3434",
+  company: {
+    name: "テスト会社名"
+  },
+  website: "https://google.com"
+};
+
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,16 +1,35 @@
-export const UserCard = () => {
+import styled from "styled-components";
+import { UserIconWidthName } from "../../../molecules/user/UserIconWidthName";
+import { Card } from "../../atoms/card/Card";
+
+export const UserCard = (props) => {
+  const { user } = props;
   return (
-    <div>
-      <img alt="プロフィール" />
-      <p>名前</p>
-      <dl>
+    <Card>
+      <UserIconWidthName image={user.image} name={user.name} />
+      <SDl>
         <dt>メール</dt>
-        <dd>TEL</dd>
-        <dt>000-999-8888</dt>
-        <dd>会社名</dd>
+        <dd>{user.email}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
         <dt>WEB</dt>
-        <dd>1111@aaaa.com</dd>
-      </dl>
-    </div>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
   );
 };
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
