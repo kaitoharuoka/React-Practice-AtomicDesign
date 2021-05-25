4284de136f462ba9f6ec1b64cb1973120a79ecf7
kaitoharuoka <73736001+kaitoharuoka@users.noreply.github.com>
2021-05-25 19:17:31 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "かいと",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@example.com",
-  phone: "080-1212-3434",
-  company: {
-    name: "テスト会社名"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
   return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,9 +1,26 @@
 import styled from "styled-components";
 
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `かいと${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "12345@example.com",
+    phone: "080-1212-3434",
+    company: {
+      name: "テスト会社名"
+    },
+    website: "https://google.com"
+  };
+});
+
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </SContainer>
   );
 };
